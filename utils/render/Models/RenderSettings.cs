71674904fb242f37ddf65a3a7e4314e2189ba509
LuanRoger <luan.roger.2003@gmail.com>
2023-12-14 15:43:23 -0300

    feat(settings): add useEnvVars  to settings

--- utils/render/Models/RenderSettings.cs
+++ utils/render/Models/RenderSettings.cs
@@ -4,4 +4,7 @@ public class RenderSettings
 {
     public required string gifskiPath { get; init; }
     public required string ffmpegPath { get; init; }
+    public required string sceneFormat { get; init; }
+    public bool useFfmpegFromEnv { get; init; }
+    public bool useGifskiFromEnv { get; init; }
 }
--- utils/render/Templates.cs
+++ utils/render/Templates.cs
@@ -5,7 +5,10 @@ public static class Templates
     public const string SETTIGNS_TEMPLATE = """
                                             {
                                                 "gifskiPath": "<PATH_TO_GIFSKI>",
-                                                "ffmpegPath": "<PATH_TO_FFMPEG>"
+                                                "ffmpegPath": "<PATH_TO_FFMPEG>",
+                                                "sceneFormat": "<SCENE_MOVIE_EXTENSION>"
+                                                "useFfmpegFromEnv": false,
+                                                "useGifskiFromEnv": false
                                             }
                                             """;
     
