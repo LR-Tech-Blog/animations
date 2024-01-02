8f86c813aa59a881d6baf5e310fe57ba7458379b
ROG <luan.roger.2003@gmail.com>
2024-01-02 10:27:13 -0300

    feat(oop_in_action): create encapsulation scene

--- animations/oop_in_action/src/code.ts
+++ animations/oop_in_action/src/code.ts
@@ -5,6 +5,22 @@ export const circleJavaClass: string = `public class Circle {
     public int possitionY;
 }`
 
+export const bookClass: string = `public class Livro {
+    private String titulo;
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+}`
+
+export const bookExternalInterface: string = `String getTitulo()
+String getAutor()`
+
 export const bookEmptyClass: string = `public class Livro {
     // O que devo colocar?
 }`
